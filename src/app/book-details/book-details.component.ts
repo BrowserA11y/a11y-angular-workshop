8848374fd0014ca571bbc6da67d304e767fb9f70
Maria Korneeva <contact@browser-person.com>
2024-08-07 18:54:42 +0200

    add rating

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book, BooksService } from '../books.service';
@@ -6,12 +6,13 @@ import { Book, BooksService } from '../books.service';
 @Component({
   selector: 'app-book-detail',
   standalone: true,
-  imports: [AsyncPipe, NgIf],
+  imports: [AsyncPipe, NgIf, NgFor],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss',
 })
 export class BookDetailComponent {
   book$!: Observable<Book>;
+  ratings = ['rating1', 'rating2', 'rating3', 'rating4', 'rating5'];
 
   constructor(private readonly bookApi: BooksService) {}
 
@@ -19,5 +20,9 @@ export class BookDetailComponent {
   set isbn(isbn: string) {
     console.log(isbn);
     this.book$ = this.bookApi.getByIsbn(isbn);
+  }
+
+  handleRating(rating: number) {
+    console.log(rating);
   }
 }
