eb9b8ca8dd6337d9a759ddd90c0d59de57c52670
Maria Korneeva <contact@browser-person.com>
2024-08-07 18:54:41 +0200

    add focus management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { filter } from 'rxjs';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, NavigationComponent],
 })
 export class AppComponent {
   //Angular focus 4: Fix navigation - focus on the first header on the page + Alternative way to handle navigation
+  constructor(private router: Router) {
+    console.log(this.router.url);
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => {
+        const main = document.querySelector('main');
+        if (main) {
+          (main as HTMLElement).focus();
+        }
+      });
+  }
 }
--- src/app/books-item/book-item.component.ts
+++ src/app/books-item/book-item.component.ts
@@ -1,16 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CdkMonitorFocus, FocusOrigin } from '@angular/cdk/a11y';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  NgZone,
+  Output,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Book } from '../books.service';
 
 @Component({
-    selector: 'app-book-item',
-    templateUrl: './book-item.component.html',
-    styleUrls: ['./book-item.component.scss'],
-    imports: [RouterLink]
+  selector: 'app-book-item',
+  templateUrl: './book-item.component.html',
+  styleUrls: ['./book-item.component.scss'],
+  imports: [CdkMonitorFocus, RouterLink],
 })
 export class BookItemComponent {
   @Input() public book?: Book;
   @Output() private bookRemoved = new EventEmitter<void>();
+  elementOrigin = this.formatOrigin(null);
+
+  constructor(private _ngZone: NgZone, private _cdr: ChangeDetectorRef) {}
+
+  formatOrigin(origin: FocusOrigin): string {
+    return origin ? origin + ' focused' : 'blurred';
+  }
+
+  // Workaround for the fact that (cdkFocusChange) emits outside NgZone.
+  markForCheck() {
+    this._ngZone.run(() => this._cdr.markForCheck());
+  }
 
   removeBook() {
     this.bookRemoved.emit();
