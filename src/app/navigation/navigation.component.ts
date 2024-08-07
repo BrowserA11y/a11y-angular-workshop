1a3ab05784f1e33681c36fe582c68bd8facb01d7
Maria Korneeva <contact@browser-person.com>
2024-08-07 18:54:42 +0200

    add hamburger navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,19 +1,39 @@
-
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 /**https://inclusive-components.design/menus-menu-buttons/ */
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss'],
-    imports: [RouterLink, RouterLinkActive]
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class NavigationComponent {
+  private router = inject(Router);
+
+  isMenuOpen = false;
   menuItems = [
     { url: 'new-book', title: 'New book' },
     { url: 'books', title: 'Books' },
     { url: 'about', title: 'About Accessible Reads' },
   ];
+
+  constructor() {
+    this.router.events.pipe(takeUntilDestroyed()).subscribe((event) => {
+      // Close the menu upon navigation
+      if (event instanceof NavigationEnd) this.isMenuOpen = false;
+    });
+  }
+
+  onButtonClick() {
+    console.log('onButtonClick');
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
