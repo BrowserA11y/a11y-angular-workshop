b08ba2457c5a41627256be05135306eb5cef84e7
Maria Korneeva <contact@browser-person.com>
2024-08-07 18:54:41 +0200

    add focus management

--- src/app/books-item/book-item.component.ts
+++ src/app/books-item/book-item.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, output } from '@angular/core';
+import { CdkMonitorFocus, FocusOrigin } from '@angular/cdk/a11y';
+import {
+  ChangeDetectorRef,
+  Component,
+  inject,
+  input,
+  NgZone,
+  output,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Book } from '../books.service';
 
@@ -6,11 +14,24 @@ import { Book } from '../books.service';
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
   styleUrls: ['./book-item.component.scss'],
-  imports: [RouterLink],
+  imports: [CdkMonitorFocus, RouterLink],
 })
 export class BookItemComponent {
+  private _ngZone = inject(NgZone);
+  private _cdr = inject(ChangeDetectorRef);
+
   public book = input.required<Book>();
   public bookRemoved = output<void>();
+  public elementOrigin = this.formatOrigin(null);
+
+  formatOrigin(origin: FocusOrigin): string {
+    return origin ? origin + ' focused' : 'blurred';
+  }
+
+  // Workaround for the fact that (cdkFocusChange) emits outside NgZone.
+  markForCheck() {
+    this._ngZone.run(() => this._cdr.markForCheck());
+  }
 
   removeBook() {
     this.bookRemoved.emit();
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,7 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { take, tap } from 'rxjs';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { Subscription, take, tap } from 'rxjs';
 import { BookItemComponent } from '../books-item/book-item.component';
 import { Book, BooksService } from '../books.service';
 
@@ -10,20 +11,50 @@ import { Book, BooksService } from '../books.service';
   styleUrls: ['./books.component.scss'],
   imports: [BookItemComponent, AsyncPipe],
 })
-export class BooksComponent {
+export class BooksComponent implements OnDestroy {
   private readonly bookService = inject(BooksService);
   #books!: Book[];
   public books$ = this.bookService.getAll().pipe(
     take(1),
     tap((books: Book[]) => (this.#books = books))
   );
+  private focusMonitor = inject(FocusMonitor);
+  private subscription = Subscription.EMPTY;
 
   removeBook(bookToRemove: Book, i: number) {
-    this.bookService
+    this.subscription = this.bookService
       .removeBook(bookToRemove)
       .pipe(take(1))
       .subscribe(() => {
         this.#books.splice(i, 1);
+        this.focusOnNextBook(i);
       });
+  }
+
+  private focusOnNextBook(i: number) {
+    // Nur in browser
+    let nextBook = document.getElementById((i + 1).toString());
+    if (!nextBook) {
+      // If there is no next element (last element deleted) take the previous one)
+      nextBook = document.getElementById((i - 1).toString());
+    }
+    if (!nextBook) {
+      // Fallback: focus on main
+      nextBook = document.getElementById('main');
+    }
+    console.log(nextBook);
+    // Was kann man denn hier focussieren?
+    // https://allyjs.io/data-tables/focusable.html
+    // Or use InteractivityChecker
+    // if (nextBook) nextBook.getElementsByTagName('button')[0].focus();
+    if (nextBook && nextBook.getElementsByTagName('button')[0])
+      this.focusMonitor.focusVia(
+        nextBook.getElementsByTagName('button')[0] as HTMLElement,
+        'keyboard'
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
